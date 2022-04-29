b13fd5d891d9d98d994a7cffba9166020cd83f91
Sadikk <youpi.killeur@gmail.com>
2022-04-29 11:30:27 +0200

    [fix] deploy

--- deploy.php
+++ deploy.php
@@ -2,9 +2,12 @@
 require 'recipe/laravel.php';
 
 // Configuration
-set('repository', 'git@github.com:TODO');
+set('repository', 'git@github.com:Sadikk/instantlyds.git');
 //set('default_stage', 'production');
 set('git_tty', true);
+set('bin/php', function () {
+    return '/usr/local/php/bin/php';
+});
 
 // [Optional] Allocate tty for git on first deployment
 set('ssh_type', 'native');
@@ -35,13 +38,13 @@ set('writable_chmod_mode', '0775');
 
 // SMART CUSTOM DEPLOY COMMANDS
 task('db:migrate', function () {
-    run("cd {{release_path}} && php artisan migrate --force");
+    run("cd {{release_path}} && /usr/local/php/bin/php artisan migrate --force");
 });
 task('group:write', function () {
     run("chmod -R g+w {{release_path}}");
 });
 task('horizon:terminate', function () {
-    run("cd {{release_path}} && php artisan horizon:terminate");
+    run("cd {{release_path}} && /usr/local/php/bin/php artisan horizon:terminate");
 })->onStage('production');
 task('supervisor:restart', function () {
     run("supervisorctl reread && supervisorctl restart all");
@@ -52,11 +55,11 @@ task('supervisor:restart', function () {
 
 host('production')
     ->stage('production')
-    ->hostname('TODO')
+    ->hostname('instantly')
     ->user('deployer')
-    ->identityFile('~/.ssh/TODO')
+    ->identityFile('~/.ssh/deployer_instantly')
     ->forwardAgent()
-    ->set('deploy_path', 'TODO');
+    ->set('deploy_path', '/data/wwwroot/leadbase.ai');
 
 
 // Run database migrations
